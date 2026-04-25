af8851cb5d060c846c7b10938aa0e29f73fd680a
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-25 10:45:28 +0530

    Update sheets.js

--- js/config/sheets.js
+++ js/config/sheets.js
@@ -1,1 +1,3 @@
-// Sheet URLs config
+export const SHEETS = {
+  CDR: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1175680150&single=true&output=csv'
+};
