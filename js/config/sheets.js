dc63c88120dd9fe23d990a64c666ac78500c9450
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-25 13:23:41 +0530

    Update sheets.js

--- js/config/sheets.js
+++ js/config/sheets.js
@@ -1,5 +1,7 @@
 export const SHEETS = {
   CDR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1175680150&single=true&output=csv",
 
+  CPR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1490735065&single=true&output=csv",
+
   PPR: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1885382311&single=true&output=csv"
 };
