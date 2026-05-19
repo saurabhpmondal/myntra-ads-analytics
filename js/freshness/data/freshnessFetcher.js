0321ea809372acd44f8f49a9b9cb8eb696ff3ec1
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-19 13:36:23 +0530

    Create data/freshnessFetcher.js

---
+++ js/freshness/data/freshnessFetcher.js
@@ -0,0 +1,44 @@
+import { SHEETS } from "../../config/sheets.js";
+
+import { fetchCSV } from "../../core/fetcher.js";
+
+import { parseCSV } from "../../core/parser.js";
+
+let READY = false;
+
+let SALES = [];
+let MASTER = [];
+
+export async function getFreshnessData() {
+
+  if (READY) {
+
+    return {
+      salesRows: SALES,
+      masterRows: MASTER
+    };
+  }
+
+  const [
+    salesCsv,
+    masterCsv
+  ] = await Promise.all([
+
+    fetchCSV(SHEETS.SALES),
+
+    fetchCSV(
+      SHEETS.PRODUCT_MASTER
+    )
+  ]);
+
+  SALES = parseCSV(salesCsv);
+
+  MASTER = parseCSV(masterCsv);
+
+  READY = true;
+
+  return {
+    salesRows: SALES,
+    masterRows: MASTER
+  };
+}
