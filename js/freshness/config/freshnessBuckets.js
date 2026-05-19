6731476608d6c3fa104e4d245ceb91bf8c00ad62
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-19 13:35:27 +0530

    Create freshness/config/freshnessBuckets.js

---
+++ js/freshness/config/freshnessBuckets.js
@@ -0,0 +1,32 @@
+export const FRESHNESS_BUCKETS = [
+
+  {
+    label: "0-30",
+    min: 0,
+    max: 30
+  },
+
+  {
+    label: "31-60",
+    min: 31,
+    max: 60
+  },
+
+  {
+    label: "61-90",
+    min: 61,
+    max: 90
+  },
+
+  {
+    label: "91-120",
+    min: 91,
+    max: 120
+  },
+
+  {
+    label: ">120",
+    min: 121,
+    max: 99999
+  }
+];
