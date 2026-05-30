641b450059af08226e27989a5b6e7e34c6c08c3c
saurabhpmondal <saurabhpmondal@gmail.com>
2026-05-30 19:00:27 +0530

    Create oos-eye/oosEyeSheets.js

---
+++ js/oos-eye/oosEyeSheets.js
@@ -0,0 +1,18 @@
+export const OOS_EYE_SHEETS = {
+
+  PRODUCT_MASTER:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=205952585&single=true&output=csv",
+
+  SJIT_STOCK:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=685171659&single=true&output=csv",
+
+  SOR_STOCK:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=2104491192&single=true&output=csv",
+
+  SELLER_STOCK:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQkr2m-hC2IUGD7YSiWr--DuhJvmmGmwMaL1ihZyKfaXHhz-YorTb_dZQVrczXTRRcxvewzojTaiqBC/pub?gid=1944471190&single=true&output=csv",
+
+  SALES:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1679615114&single=true&output=csv"
+
+};
